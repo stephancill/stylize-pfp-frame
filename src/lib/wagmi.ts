174293d103c0e1fe24a5ad47f2e57ee193844df3
Stephan Cilliers <stephan.cilliers@coinbase.com>
2025-06-10 12:44:45 +0200

    feat: wallet connectors

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,7 +1,14 @@
 import { createConfig, http } from "wagmi";
 import { base } from "wagmi/chains";
+import { injected, coinbaseWallet } from "@wagmi/connectors";
 
 export const config = createConfig({
+  connectors: [
+    coinbaseWallet({
+      appName: "Stylize Me",
+      appLogoUrl: "https://stylizeme.xyz/splash.png",
+    }),
+  ],
   chains: [base as any],
   transports: {
     [base.id]: http(),
